811504ad260101d64f2de991d2b31738a2b10ed3
meschke <tim.mesche@hotmail.de>
2016-02-26 12:56:48 +0100

    Update LivingBeing.java
    Added attributes and getters/setters.

--- Text-based-Gothic/src/tm/textbasedgothic/main/LivingBeing.java
+++ Text-based-Gothic/src/tm/textbasedgothic/main/LivingBeing.java
@@ -1,5 +1,72 @@
 package tm.textbasedgothic.main;
 
 public class LivingBeing {
+  protected final String name;
+  protected final String description;
+  protected int level;
+  protected int health;
+  protected int maxHealth;
+  protected int strength;
+  protected int agility;
+//  private Inventory inventory;
+//  private Skill skills;                 // maybe more like List<Skill> skills;
+//  Attack Power and Resistances
+  
+  // Setters and Getters
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getDescription() {
+    return description;
+  }
+  
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  
+  public String getLevel() {
+    return level;
+  }
+  
+  public void setLevel(String level) {
+    this.level = level;
+  }
+  
+  public String getHealth() {
+    return health;
+  }
+  
+  public void setHealth(String health) {
+    this.health = health;
+  }
+  
+  public String getMaxHealth() {
+    return maxHealth;
+  }
+  
+  public void setMaxHealth(String maxHealth) {
+    this.maxHealth = maxHealth;
+  }
+  
+  public String getStrength() {
+    return strength;
+  }
+  
+  public void setStrength(String strength) {
+    this.strength = strength;
+  }
+
+  public String getAgility() {
+    return agility;
+  }
+  
+  public void setAgility(String agility) {
+    this.agility = agility;
+  }
 
 }
