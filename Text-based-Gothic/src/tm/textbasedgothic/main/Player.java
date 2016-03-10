9c9b48703e64adfd4ec5a6ba9a08ad00a4a56377
meschke <tim.mesche@hotmail.de>
2016-03-10 16:00:36 +0100

    Update Player.java

--- Text-based-Gothic/src/tm/textbasedgothic/main/Player.java
+++ Text-based-Gothic/src/tm/textbasedgothic/main/Player.java
@@ -1,11 +1,6 @@
 package tm.textbasedgothic.main;
 
 public class Player extends LivingBeing {
-	// Additional attributes
-	private int exp;
-	private int maxExp;		// to reach new level
-	private int lp;			// good old Gothic-tradition: Learning Points!
-	
 	// Starting stats for Hero
 	final private String INIT_HERO_NAME = "Nameless";	// name
 	final private int INIT_HERO_LEVEL = 1;			// level
@@ -15,6 +10,52 @@ public class Player extends LivingBeing {
 	final private int INIT_HERO_STR = 10;			// strength
 	final private int INIT_HERO_AGI = 10;			// agility
 	final private int INIT_HERO_LP = 0;			// learning points
+	
+	// Additional attributes
+	private int exp;
+	private int maxExp;		// to reach new level
+	private int lp;			// good old Gothic-tradition: Learning Points!
+	
+	// Hero when starting new game
+	private Player() {
+		// inherited attributes
+		name = INIT_HERO_NAME;
+		level = INIT_HERO_LEVEL;
+		health = INIT_HERO_HP;
+		amxHealth = INIT_HERO_HP;
+		strength = INIT_HERO_STR;
+		agility = INIT_HERO_AGI;
+		currentLocation = new Location(startingLocation);
+		// own attributes
+		exp = INIT_HERO_EXP;
+		maxExp = INIT_HERO_MAXEXP;
+		lp = INIT_HERO_LP;
+	}
+	
+	// Getters / setters
+	private void setExp(int exp) {
+		this.exp = exp;
+	}
+	
+	private int getExp() {
+		return this.exp;
+	}
+	
+	private void setMaxExp(int maxExp) {
+		this.maxExp = maxExp;
+	}
+	
+	private int getMaxExp() {
+		return this.maxExp;
+	}
+	
+	private void setLP(int lp) {
+		this.lp = lp;
+	}
+	
+	private int getLP() {
+		return this.lp;
+	}
 	
 	
 }
