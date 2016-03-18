c6ef567c8c50a609ca0ea42d257b705259f51411
meschke <tim.mesche@hotmail.de>
2016-03-18 10:28:47 +0100

    Finishing constructor Window()
    Opening the window will not initialize the game, but will view the title screen first.

--- Text-based-Gothic/src/tm/textbasedgothic/main/Window.java
+++ Text-based-Gothic/src/tm/textbasedgothic/main/Window.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Window extends JFrame {
 	
-	public Window (int height, int width, int title, Game game) {
+	public Window (int height, int width, String title, Game game) {
 		JFrame frame = new JFrame(title);
 		
 		frame.setPreferredSize(new Dimension(width, heigth));
@@ -12,6 +12,11 @@ public class Window extends JFrame {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
+		
 		frame.add(game);
+		
+		frame.setVisible(true);
+		
+		new ViewMenu (game, this);
 	}
 }
